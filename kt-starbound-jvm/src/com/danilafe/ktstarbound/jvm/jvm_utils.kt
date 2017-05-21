e199e49e955153571d45083b02f62997b88cd5d4
Danila Fedorin <danila.fedorin@gmail.com>
2017-05-21 00:09:08 -0700

    Implement JVM functions to use with the World class.

--- kt-starbound-jvm/src/com/danilafe/ktstarbound/jvm/jvm_utils.kt
+++ kt-starbound-jvm/src/com/danilafe/ktstarbound/jvm/jvm_utils.kt
@@ -1,16 +1,36 @@
 @file:JvmName("JvmUtils")
 package com.danilafe.ktstarbound.jvm
 
+import com.danilafe.ktstarbound.extractor.GenericExtractor
 import com.danilafe.ktstarbound.jvm.extractor.JvmExtractor
 import com.danilafe.ktstarbound.jvm.readers.RandomLeafReader
 import com.danilafe.ktstarbound.jvm.readers.RandomReader
 import com.danilafe.ktstarbound.readers.GenericLeafReader
 import com.danilafe.ktstarbound.readers.GenericReader
+import com.danilafe.ktstarbound.readers.impl.ArrayReader
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.nio.ByteBuffer
+import java.util.zip.Inflater
 
 fun readRawInt(array: ByteArray): Int {
     return ByteBuffer.wrap(array).int
+}
+
+fun decompress(array: ByteArray): ByteArray {
+    val inflater = Inflater()
+    val outputStream = ByteArrayOutputStream()
+    val buffer = ByteArray(2048)
+    inflater.setInput(array)
+    while(!inflater.finished()){
+        val readBytes = inflater.inflate(buffer)
+        outputStream.write(buffer)
+    }
+    return outputStream.toByteArray()
+}
+
+fun createArrayExtractor(reader: ArrayReader): GenericExtractor<GenericReader> {
+    return JvmExtractor(reader)
 }
 
 fun createLeafExtractor(file: String, headerSize: Long, blockSize: Long, prefixSize: Long): JvmExtractor<GenericLeafReader> {
