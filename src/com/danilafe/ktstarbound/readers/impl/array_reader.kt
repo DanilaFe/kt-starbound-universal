3ebc837df1dc10b44d7fd805f39477d0e2abe3e6
Danila Fedorin <danila.fedorin@gmail.com>
2017-05-21 12:58:57 -0700

    Change variable name to prevent conflicts with index().

--- src/com/danilafe/ktstarbound/readers/impl/array_reader.kt
+++ src/com/danilafe/ktstarbound/readers/impl/array_reader.kt
@@ -7,27 +7,27 @@ import com.danilafe.ktstarbound.readers.GenericReader
  * As opposted to Platform-specific readers, this one is
  * meant to be used for data that has already been decoded by something else.
  */
-public class ArrayReader(val array: ByteArray, var index: Long) : GenericReader() {
+public class ArrayReader(val array: ByteArray, var arrayIndex: Long) : GenericReader() {
 
     override fun index(): Long {
-        return index
+        return arrayIndex
     }
 
     override fun read(length: Int): ByteArray? {
         val bytes = ByteArray(length)
         for(i in 0..length - 1){
-            bytes[i] = array[(index + i).toInt()]
+            bytes[i] = array[(arrayIndex + i).toInt()]
         }
         advance(length.toLong())
         return bytes
     }
 
     override fun advance(by: Long) {
-        move(index + by)
+        move(arrayIndex + by)
     }
 
     override fun move(to: Long) {
-        index = to
+        arrayIndex = to
     }
 
 }
